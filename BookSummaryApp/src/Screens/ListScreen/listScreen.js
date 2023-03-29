9ad867b7e98facc9164bbad9cbf60a8a74bfa6e6
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-29 13:34:54 +0300

    Created UseFetch Hook

--- BookSummaryApp/src/Screens/ListScreen/listScreen.js
+++ BookSummaryApp/src/Screens/ListScreen/listScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, } from "react-native";
+import { View, FlatList } from "react-native";
 import BookCard from "../../components/BookCard";
 import styles from './listScreen.style';
 
 function listScreen () {
     return (
         <View style = {styles.container}>
-            <BookCard></BookCard>
+            <FlatList ></FlatList>
         </View>
     )
 }
