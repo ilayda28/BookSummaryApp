1a8eba7e437ddbe694529f35ba19787c452d037a
İlayda Sena Keleş <ilaydasenakeles28@gmail.com>
2023-03-29 13:35:15 +0300

    Created UseFecth Hook

---
+++ BookSummaryApp/src/Hook/UseFetch/UseFetch.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useState, useEffect} from "react";
+
+function UseFetch(url) {
+    const [ data, setData ] = useState();
+    const fetchData =async ()=> {
+        try {
+            const {responseData} = await axios.get(url)
+            setData (responseData)
+        } catch (error) {
+            console.log (error)
+        }
+    }
+    useEffect (()=> {
+        fetchData(), []
+    }) 
+    return data;
+}
+
+export default UseFetch;
